2617dbf25cecf46168c2d139c863e8006ed26fa3
AdiyoFouad <odjfouad38@gmail.com>
2025-07-28 21:03:42 +0100

    navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './pages/task-list/task-list.component';
+import { TaskComponent } from './pages/task/task.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HabitComponent } from './pages/habit/habit.component';
+import { BudgetComponent } from './pages/budget/budget.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    component: DashboardComponent
+}, {
+    path:'tasks',
+    component: TaskListComponent
+}, {
+    path: 'task',
+    children : [{
+        path:'',
+        component: TaskComponent
+    }, {
+        path:':id',
+        component: TaskComponent
+    }]
+}, {
+    path:'budget',
+    component: BudgetComponent
+}, {
+    path:'habits',
+    component: HabitComponent
+}, {
+    path:'settings',
+    component: SettingsComponent
+}
+];
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit, Signal, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule, MatNavList } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Subject, Subscription, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet, 
+    RouterLink,
+    RouterLinkActive,
+    MatSidenavModule, 
+    MatListModule,
+    MatToolbarModule, 
+    MatIconModule, 
+    MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('LifeBoard');
+export class App implements OnInit, OnDestroy{
+  isMobile = signal(false);
+  private breakpointObserver = inject(BreakpointObserver);
+  private subscriptions = new Subscription();
+
+  ngOnInit(): void {
+
+    const breakpointSubscription =  this.breakpointObserver.observe(['(max-width: 768px)']).subscribe((result) => {
+      this.isMobile.set(result.matches);
+    });
+    this.subscriptions.add(breakpointSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  
 }
